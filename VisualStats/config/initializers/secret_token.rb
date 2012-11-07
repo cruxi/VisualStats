d20120715f9b47f270880d8bb329265c2d5bf631
Kristina Kirsten <kristina_kirsten@gmx.de>
2012-11-07 13:18:19 +0100

    Project created

---
+++ VisualStats/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisualStats::Application.config.secret_token = 'dad984b8e4c24972174f22c99b7643b574e767fdce9d7f36cc8491ec721d903a664d366f13da3d9c905eefbf55a18074cd4398f6469462450a1f628a80614e89'
