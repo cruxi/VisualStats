547a3a132b25b4e1525b7b7faa07f9204c55b533
jt <memetical@googelmail.com>
2012-12-05 13:03:21 +0100

    Created a new parsecfg2-method which prints out all keys and values.

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,17 @@ class Job < ActiveRecord::Base
 
   belongs_to :repository, :foreign_key => "repository_id"
 
+
+  # the known versions of **some** of the runtimes.
+  KNOWN_RUBY_VERSIONS   = %w(1.8.7 ruby-1.8.7 1.9.2 ruby-1.9.2 1.9.3 ruby-1.9.3 ruby-head jruby jruby-18mode jruby-19mode rbx rbx-18mode rbx-19mode jruby-head ree ree-1.8.7)
+  KNOWN_NODE_VERSIONS   = %w(0.6 0.8 0.9)
+  KNOWN_PHP_VERSIONS    = %w(5.2 5.3 5.3.2 5.3.8 5.4)
+  KNOWN_PYTHON_VERSIONS = %w(2.5 2.6 2.7 3.2)
+  KNOWN_PERL_VERSIONS   = %w(5.10 5.12 5.14 5.16)
+
+  @KNOWN_LANGUAGES = %w(ruby java python)
+
+
   # workaround since :type is a reserved keyword by ActiveRecord::Base.
   # Please see http://www.ruby-forum.com/topic/101557 for further details.
   set_inheritance_column :ruby_type
@@ -19,7 +30,7 @@ class Job < ActiveRecord::Base
 
 
 
-  # this is used to retrieve the respective models config file
+  # this is used to retrieve the objects config file
   # If no value has been set for the language field, we assume ruby as the language.
   def parsecfg
 
@@ -27,24 +38,78 @@ class Job < ActiveRecord::Base
 
     begin
       language = yaml.fetch(:language)
-      pp language
+      runtime = yaml.keys[1]
+      version = yaml.values[1]
     rescue
       language = "ruby"
       pp "Assigned: #{language}"
     ensure
       pp "Language is: #{language}"
-    end
-
-
-    begin
-      version = yaml.values[1] # we cannot look for the hash key here, since it is sometimes present and sometimes not. Position of the key should stay the same though        pp language
-    rescue
-    ensure
       pp "Version is: #{version}"
     end
-
-
   end
+
+  # this is used to retrieve the objects config file
+  # If no value has been set for the language field, we assume ruby as the language.
+  def parsecfg2
+
+    yaml = YAML.load(config)
+
+    # !Nicht vergessen values sind hier immer strings!
+    begin
+      # p "yaml.keys[0]: #{yaml.keys[0]}"
+      # p "yaml.values[0]: #{yaml.values[0]}"
+      # p "yaml.keys[1]: #{yaml.keys[1]}"
+      # p "yaml.values[1]: #{yaml.values[1]}"
+       
+      # Checking via intersection whether we have a matching key or value
+      #lkey   = [yaml.keys[0]] & [:language]
+      #lvalue = [yaml.values[0]] & ['python', 'ruby']
+
+      lkey   = [yaml.keys[0]] 
+      lvalue = [yaml.values[0]] 
+      rkey   = [yaml.keys[1]] 
+      rvalue = [yaml.values[1]]
+      vkey   = [yaml.keys[2]] 
+      vvalue = [yaml.values[2]]
+
+   
+
+      pp "language key: #{lkey}"
+      pp "language value: #{lvalue}"
+
+      pp "runtime key: #{rkey}"
+      pp "runtime value: #{rvalue}"
+
+      pp "version key: #{vkey}"
+      pp "version value: #{vvalue}"
+
+      
+      p "\n"
+    
+
+      #runtime = [yaml.keys[1]] & [:rvm:, :ruby:]
+      #p "Runtime: #{runtime}"
+      #p version = [yaml.values[1]] & KNOWN_VERSIONS
+      #language = yaml.fetch(:language)
+      #runtime = yaml.keys[1]
+      #version = yaml.values[1]
+
+      # rescue
+      #   language = "ruby"
+      #   pp "Assigned: #{language}"
+      # ensure
+      #   pp "Language is: #{language}"
+      #   #pp "Version is: #{version}"
+      # end
+    end
+  end
+
+  def prnts
+    cfg = YAML.load(config)
+    pp "Language: #{cfg.values[0]} Runtime: #{cfg.keys[1]} Version: #{cfg.values[1]}"
+  end
+
 
   #Limit the rows to be shown on one page
   self.per_page = 10
