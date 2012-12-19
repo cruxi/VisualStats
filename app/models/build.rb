9d48959791f74a8a780abf6c89c5f365ba9edce6
Kristina Kirsten <kristina_kirsten@gmx.de>
2012-12-19 14:43:37 +0100

    statistics for start-page-overview

--- app/models/build.rb
+++ app/models/build.rb
@@ -10,4 +10,48 @@ class Build
 
  #Limit the rows to be shown on one page
  self.per_page = 3
+
+# CLASS METHODS
+def self.getFailedAmount
+	failedAmount = Build.where("result = ?", 0).count(:result)
+	return failedAmount
+end
+
+def self.getSuccessAmount
+	successAmount = Build.where("result = ?", 1).count(:result)
+	return successAmount
+end
+
+def self.getNotFinishedAmount
+	notFinishedAmount = Build.where("duration < 1").count(:result)
+	return notFinishedAmount
+end
+
+def self.getLanguagesAmount
+	amount = Hash.new 
+	s = Build::ENV_KEYS
+
+ 	s.each { |lang| 
+ 		tempLangAmount = Build.where("config like ?","%#{lang}%").count() 
+ 		amount[lang]=tempLangAmount
+ 	}
+
+ 	return amount
+end
+
+def self.getLanguges 
+	s = Build::ENV_KEYS
+	languages = Array.new
+
+	s.each { |lang| 
+		languages << lang.to_s
+	}
+	return languages
+end	
+
+
+
+#INSTANCE METHODS
+
+
 end
