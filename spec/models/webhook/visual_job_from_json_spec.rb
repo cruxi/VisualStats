f80288a9c79db8a0c0934036fdd434696d8148e6
Dr Blinken <drblinken@gmail.com>
2013-01-08 13:32:19 +0100

    some spec cleanup

--- spec/models/webhook/visual_job_from_json_spec.rb
+++ spec/models/webhook/visual_job_from_json_spec.rb
@@ -17,22 +17,13 @@ describe VisualJob do
        subject { visual_build_failed.jobs.first }
        describe "with fields" do
           its(:travis_id) { should == 3628507 }
-         # its(:repository_id) { should == 366197 }
           its(:number) { should == "16.1" }
           its(:state) { should == "finished" }
           its(:result) { should == 1 }
           its(:finished_at) { should == "2012-12-12T13:47:48Z" }
           its(:build) { should == visual_build_failed }
           its(:allow_failures) { should == false }
-    # its(:dimensions) { should == {rvm: "1.9.3",env:"DB=sqlite" } }
-      it "should preserve the original ids - at least for build"
-         # its(:id) { should == 3628507 }
-
-
-      end
-      it "handles missing fields gracefully"
-      it "logs a warning if a field is missing"
-      it "should preserve the original ids - at least for build"
+        end
     end
  describe "dimensions" do
       it "should be 2" do
