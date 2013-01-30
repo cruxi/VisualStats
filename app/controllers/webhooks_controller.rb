8b3dfb9712accf1457a72849343bab9c36e1f16b
Dr Blinken <drblinken@gmail.com>
2013-01-30 15:02:51 +0100

    webhook in progress

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,3 +1,28 @@
 class WebhooksController < ApplicationController
-
+  # POST /notify/:username/:repository
+  def notify
+    username = params[:username]
+    repository =  params[:repository]
+    payload = params[:payload]
+    authorized = authorize_notification(username,repository,request.headers['Authorization'])
+   # @call_back = CallBack.new(params) would probably work, too
+    @visual_build = VisualBuild.create_from_json_str(payload)
+    respond_to do |format|
+      if @visual_build.save
+        format.html { redirect_to @visual_build, notice: 'Build was successfully created.' }
+        format.json { render json: @visual_build, status: :created, location: @visual_build }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @visual_build.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def authorize_notification(username,repository,sha256)
+    token = "h5Rpd7dqe2zbV8XFawzo"
+    hash =  Digest::SHA256.hexdigest("#{username}/#{repository}#{token}")
+    return (hash == sha256)
+    #note = (hash == sha256) ? "AUTHORIZED " : "NOT AUTHORIZED "
+    #note += sha256 if sha256
+    #note
+  end
 end
--- spec/controllers/webhooks_controller_spec.rb
+++ spec/controllers/webhooks_controller_spec.rb
@@ -2,4 +2,56 @@ require 'spec_helper'
 
 describe WebhooksController do
 
+  def valid_attributes
+    { "username" => "DrBlinken" }
+  end
+
+  def valid_session
+    {}
+  end
+
+  def payload
+     payload = <<DELIM
+     {"id":4291240,"repository":{"id":366890,"name":"WebHooksPlayground","owner_name":"bkleinen","url":"https://github.com/bkleinen/WebHooksPlayground"},"number":"11","config":{"language":"ruby","rvm":["1.9.3"],"env":["DB=sqlite"],"script":["RAILS_ENV=test bundle exec rake --trace db:schema:load","RAILS_ENV=test bundle exec rspec spec"],"notifications":{"webhooks":["http://visualstats.herokuapp.com/bkleinen/WebHooksPlayground/notify"]},".result":"configured"},"status":0,"result":0,"status_message":"Fixed","result_message":"Fixed","started_at":"2013-01-21T21:14:39Z","finished_at":"2013-01-21T21:15:42Z","duration":63,"build_url":"https://travis-ci.org/bkleinen/WebHooksPlayground/builds/4291240","commit":"a1e696e84035de9037c843f837faa88badd7c7dd","branch":"master","message":"restoring travis.yml","compare_url":"https://github.com/bkleinen/WebHooksPlayground/compare/e8ebf6466b39...a1e696e84035","committed_at":"2013-01-21T21:07:57Z","author_name":"Dr Blinken","author_email":"drblinken@gmail.com","committer_name":"Dr Blinken","committer_email":"drblinken@gmail.com","matrix":[{"id":4291241,"repository_id":366890,"parent_id":4291240,"number":"11.1","state":"finished","config":{"language":"ruby","rvm":"1.9.3","env":"DB=sqlite","script":["RAILS_ENV=test bundle exec rake --trace db:schema:load","RAILS_ENV=test bundle exec rspec spec"],"notifications":{"webhooks":["http://visualstats.herokuapp.com/bkleinen/WebHooksPlayground/notify"]},".result":"configured"},"status":0,"result":0,"commit":"a1e696e84035de9037c843f837faa88badd7c7dd","branch":"master","message":"restoring travis.yml","compare_url":"https://github.com/bkleinen/WebHooksPlayground/compare/e8ebf6466b39...a1e696e84035","committed_at":"2013-01-21T21:07:57Z","author_name":"Dr Blinken","author_email":"drblinken@gmail.com","committer_name":"Dr Blinken","committer_email":"drblinken@gmail.com","finished_at":"2013-01-21T21:15:42Z"}]}
+DELIM
+  end
+
+  def notify_params
+    { username: "drblinken", repository: "VisualStats", payload: payload}
+  end
+  describe "double check: does the payload work?" do
+    it "builds a visual build" do
+      visual_build = VisualBuild.create_from_json_str(payload)
+      visual_build.should_not == nil
+      visual_build.jobs.size.should == 1
+    end
+  end
+  describe "notify" do
+      describe "POST notify" do
+         describe "with valid params" do
+           it "creates a new VisualBuild" do
+            pending
+             expect {
+               post :notify, notify_params, valid_session
+             }.to change(VisualBuild, :count).by(1)
+           end
+
+           it "assigns a newly created call_back as @call_back" do
+            pending
+             post :notify, notify_params, valid_session
+             assigns(:call_back).should be_a(CallBack)
+             assigns(:call_back).should be_persisted
+           end
+
+           it "redirects to the created call_back" do
+            pending
+             post :notify, notify_params, valid_session
+             response.should redirect_to(CallBack.last)
+           end
+         end
+
+
+       end
+
+  end
 end
